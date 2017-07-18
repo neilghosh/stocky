e18ae5fb090c5270a7994675fd5a21aa64e7f68d
Neil Ghosh <sabyasachi.ghosh@jda.com>
2017-07-19 00:26:00 +0530

    Added endpoint for fetching historical data

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from google.appengine.api import urlfetch
 import re
 import json
 import urllib
+import csv, io
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -63,7 +64,41 @@ class QuotePage(webapp2.RequestHandler):
             logging.exception('Caught exception fetching url')
         self.response.headers['Content-Type'] = 'application/json'
 
+class HistoricalDataPage(webapp2.RequestHandler):
+    def get(self):
+        name = self.request.get("name")
+        startDate = self.request.get("startdate")
+        endDate = self.request.get("enddate")
+        datePeriod = self.request.get("dateperiod")
+        ## url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxGetQuoteJSON.jsp?symbol='+name.replace(" ", "%20")+'&series=EQ'
+        ##         url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/getHistoricalData.jsp?symbol='+name+'&series=EQ&fromDate='+startDate+'&toDate='+endDate+''
+        ## url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/getHistoricalData.jsp?symbol='+name+'&series=EQ&fromDate=undefined&toDate=undefined&datePeriod=3months&hiddDwnld=true'
+        ##url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/getHistoricalData.jsp?symbol='+name+'&series=EQ&fromDate=01-Jul-2017&toDate=13-Jul-2017'
+        url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/getHistoricalData.jsp?symbol='+name+'&series=EQ&fromDate='+startDate+'&toDate='+endDate+'&datePeriod='+datePeriod+''
+        ## http://localhost:8080/getdata?name=TCS&startdate=01-07-2016&enddate=18-07-2017&dateperiod=3months
+        print url
+        try:
+            result = urlfetch.fetch(url)
+            print result
+            if result.status_code == 200:
+                regex = '"Date".*:'
+                pattern = re.compile(regex)
+                data = re.findall(pattern, result.content)[0].replace(':','\n')
+                print data
+                ##fieldnames = ("Date","Symbol","Series","Open Price","High Price","Low Price","Last Traded Price ","Close Price","Total Traded Quantity","Turnover (in Lakhs)")
+                reader = csv.DictReader(io.StringIO(unicode(data)))
+                json_data = json.dumps(list(reader))
+                self.response.write(json_data)
+            else:
+                self.response.status_code = result.status_code
+        except urlfetch.Error:
+            logging.exception('Caught exception fetching url')
+        self.response.headers['Content-Type'] = 'application/json'
+
+
+
 app = webapp2.WSGIApplication([
     ('/companysearch', MainPage),
     ('/getquote', QuotePage),
+    ('/getdata', HistoricalDataPage),
 ], debug=True)
